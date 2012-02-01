d4f78030a11791d31b066f8cb18bea6d9a872520
Juanje Ojeda <juanje.ojeda@gmail.com>
2012-02-01 03:32:36 +0000

    Changed the 'sudo' command by the execute's attribute 'user'

--- providers/gconf.rb
+++ providers/gconf.rb
@@ -1,17 +1,19 @@
-def gconftool(username)
-  "xvfb-run -w 0 sudo -iu #{username} gconftool-2"
+def gconftool
+  "xvfb-run -w 0 gconftool-2"
 end
 
 action :set do
   execute "set key" do
-    command "#{gconftool(new_resource.username)} --set #{new_resource.name} #{new_resource.value} --type #{new_resource.type}"
+    user    new_resource.username
+    command "#{gconftool} --set #{new_resource.name} #{new_resource.value} --type #{new_resource.type}"
   end
   new_resource.updated_by_last_action(true)
 end
 
 action :unset do
   execute "unset key" do
-    command "#{gconftool(new_resource.username)} --unset #{new_resource.name}"
+    user    new_resource.username
+    command "#{gconftool} --unset #{new_resource.name}"
   end
   new_resource.updated_by_last_action(true)
 end
--- providers/gsettings.rb
+++ providers/gsettings.rb
@@ -1,17 +1,19 @@
-def gsettings(username)
-  "xvfb-run -w 0 sudo -iu #{username} gsettings"
+def gsettings
+  "xvfb-run -w 0 gsettings"
 end
 
 action :set do
   execute "set key" do
-    command "#{gsettings(new_resource.username)} set #{new_resource.schema} #{new_resource.name} #{new_resource.value}"
+    user    new_resource.username
+    command "#{gsettings} set #{new_resource.schema} #{new_resource.name} #{new_resource.value}"
   end
   new_resource.updated_by_last_action(true)
 end
 
 action :unset do
   execute "unset key" do
-    command "#{gsettings(new_resource.username)} reset #{new_resource.schema} #{new_resource.name}"
+    user    new_resource.username
+    command "#{gsettings} reset #{new_resource.schema} #{new_resource.name}"
   end
   new_resource.updated_by_last_action(true)
 end
