3ad0e2484bc65aba122f47e2ca19bf147f243f23
Juanje Ojeda <juanje.ojeda@gmail.com>
2012-02-03 05:32:53 +0000

    Upgrade the patch version as long as an blocker bug was fixed

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name              "desktop"
-version           "0.1.0"
+version           "0.1.1"
 maintainer        "Juanje Ojeda"
 maintainer_email  "juanje.ojeda@gmail.com"
 license           "Apache 2.0"
